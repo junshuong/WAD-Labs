656ae5fc543baca47085acbbb47056821a1ea8d4
Jun-Shuo Ng <20069422@mail.wit.ie>
2021-10-29 20:29:55 +0100

    Arrays.

--- lab001/fundamentals.js
+++ lab001/fundamentals.js
@@ -1,5 +1,10 @@
 const aCar = {
     owner : "Joe Bloggs",
+    address : '3 Walkers Lane',
+    previous_owners: [
+      { name : "Pat Smith", address : "1 Main Street" },
+      { name : "Sheila Dwyer", address : "2 High Street"}
+    ],
     type : { 
       make : "Toyota", 
       model : 'Corolla', 
@@ -9,26 +14,31 @@ const aCar = {
   };
   
   console.log(aCar.owner);
-  // Nested objects
-  console.log(aCar.owner + ' drives a ' + aCar.type.make)
-  console.log(' Reg. = ' + aCar.registration.year + '-' + aCar.registration.county + '-' + aCar.registration.number)
-  // Dynamic Properties
-  aCar.milage = 10000;
-  aCar.colour = {
-    exterior : "red",
-    interior : {
-      texture : "leather",
-      shade : "cream"
-    }
-  }
-  console.log(
-    "It is a " +
-      aCar.colour.exterior +
-      " car, " +
-      aCar.milage +
-      " milage, with " +
-      aCar.colour.interior.texture +
-      " interior."
-  );
+  
+  // ===== Nested objects =====
+  // console.log(aCar.owner + ' drives a ' + aCar.type.make)
+  // console.log(' Reg. = ' + aCar.registration.year + '-' + aCar.registration.county + '-' + aCar.registration.number)
+  
+  // ===== Dynamic Properties =====
+  // aCar.milage = 10000;
+  // aCar.colour = {
+  //   exterior : "red",
+  //   interior : {
+  //     texture : "leather",
+  //     shade : "cream"
+  //   }
+  // }
+  // console.log(
+  //   "It is a " +
+  //     aCar.colour.exterior +
+  //     " car, " +
+  //     aCar.milage +
+  //     " milage, with " +
+  //     aCar.colour.interior.texture +
+  //     " interior."
+  // );
+
+  // ===== Arrays =====
+  console.log('First owner : ' + aCar.previous_owners[0].name + " - " + aCar.previous_owners[0].address );
 
   
