b149117cac44f47bf9d8a50f77841c186b7f0f66
Jun-Shuo Ng <20069422@mail.wit.ie>
2021-10-29 20:06:18 +0100

    Dynamic Properties.

--- lab001/fundamentals.js
+++ lab001/fundamentals.js
@@ -9,6 +9,26 @@ const aCar = {
   };
   
   console.log(aCar.owner);
+  // Nested objects
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
   console.log(' Reg. = ' + aCar.registration.year + '-' + aCar.registration.county + '-' + aCar.registration.number)
+  // Dynamic Properties
+  aCar.milage = 10000;
+  aCar.colour = {
+    exterior : "red",
+    interior : {
+      texture : "leather",
+      shade : "cream"
+    }
+  }
+  console.log(
+    "It is a " +
+      aCar.colour.exterior +
+      " car, " +
+      aCar.milage +
+      " milage, with " +
+      aCar.colour.interior.texture +
+      " interior."
+  );
+
   
