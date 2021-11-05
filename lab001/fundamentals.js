cf153e4e8bc6207e5bf3b8a767d75b4ac9cf3ba1
Jun-Shuo Ng <20069422@mail.wit.ie>
2021-11-05 17:57:43 +0000

    Initial structure.

--- lab001/fundamentals.js
+++ lab001/fundamentals.js
@@ -16,31 +16,31 @@ const aCar = {
   
   console.log(aCar.owner);
   
-  // // ===== Nested objects =====
-  // console.log(aCar.owner + ' drives a ' + aCar.type.make)
-  // console.log(' Reg. = ' + aCar.registration.year + '-' + aCar.registration.county + '-' + aCar.registration.number)
+  // ===== Nested objects =====
+  console.log(aCar.owner + ' drives a ' + aCar.type.make)
+  console.log(' Reg. = ' + aCar.registration.year + '-' + aCar.registration.county + '-' + aCar.registration.number)
   
-  // // ===== Dynamic Properties =====
-  // aCar.milage = 10000;
-  // aCar.colour = {
-  //   exterior : "red",
-  //   interior : {
-  //     texture : "leather",
-  //     shade : "cream"
-  //   }
-  // }
-  // console.log(
-  //   "It is a " +
-  //     aCar.colour.exterior +
-  //     " car, " +
-  //     aCar.milage +
-  //     " milage, with " +
-  //     aCar.colour.interior.texture +
-  //     " interior."
-  // );
+  // ===== Dynamic Properties =====
+  aCar.milage = 10000;
+  aCar.colour = {
+    exterior : "red",
+    interior : {
+      texture : "leather",
+      shade : "cream"
+    }
+  }
+  console.log(
+    "It is a " +
+      aCar.colour.exterior +
+      " car, " +
+      aCar.milage +
+      " milage, with " +
+      aCar.colour.interior.texture +
+      " interior."
+  );
 
-  // // ===== Arrays =====
-  // console.log('First owner : ' + aCar.previous_owners[0].name + " - " + aCar.previous_owners[0].address );
+  // ===== Arrays =====
+  console.log('First owner : ' + aCar.previous_owners[0].name + " - " + aCar.previous_owners[0].address );
   
   // ===== Looping/Iterations constructs =====
   // Loop through each car feature
